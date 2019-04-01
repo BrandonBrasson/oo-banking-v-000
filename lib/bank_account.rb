bd907816d31ae2441afe2a4bbbd4581987748971
Brandon Brasson <rocksong622@gmail.com>
2019-04-01 15:06:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,57 @@
 class BankAccount
+  attr_accessor :balance, :status
+   attr_reader :name
+
+   def initialize(name)
+     @name = name
+     @balance = 1000
+     @status = "open"
+   end
+
+   def deposit(amount)
+     self.balance += amount
+   end
+
+   def display_balance
+    p "Your balance is $#{self.balance}."
+   end
+
+   def valid?
+     self.balance > 0 && self.status == "open"
+   end
+
+  #  if self.balance > 0
+    # return self,balance
+    #  else self.status == "open"
+   #return self.status
+   #end
+#end
+
+   def close_account
+     self.status = "closed"
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+    def initialize(name)
+      @sender = sender
+      @receiver = receiver
+      @status = "pending"
+      @amount = amount
+    end
+
+    def valid?
+      sender.valid? && receiver.valid?
+    end
+
+
+
+
+
 end
