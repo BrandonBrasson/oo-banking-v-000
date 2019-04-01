42d44e93f1c7fb7720e1b114b3f8efecef830688
Brandon Brasson <rocksong622@gmail.com>
2019-04-01 15:41:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,17 +6,29 @@ class Transfer
      @receiver = receiver
      @amount = amount
      @status = "pending"
+
    end
 
    def valid?
-     @sender.valid? && @receiver.valid? ? true : false
+     @sender.valid? && @receiver.valid?
    end
 
 
+     def execute_transaction
+       if @sender.balance > @amount && @status == "pending"
+         @sender.balance -= @amount
+         @receiver.balance += @amount
+         @status = "complete"
+       else
+         @status = "rejected"
+         return "Transaction rejected. Please check your account balance."
+       end
+     end
+
    def reverse_transfer
     if @status == "complete"
-      @sender.balance += amount
-      self.receiver.balance -= amount
+      @sender.balance += @amount
+      self.receiver.balance -= @amount
       @status = "reversed"
     end
   end
